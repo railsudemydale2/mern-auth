0d0fc7fec4174140077e4c0caf116bfcb5f120bd
dale <stewartdale45@yahoo.com>
2023-09-13 22:28:35 -0400

    create sign in api route

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqFuBrMYk2UAvH8P9qtQzRom52FIzT0eo",
+  authDomain: "mern-auth-4b709.firebaseapp.com",
+  projectId: "mern-auth-4b709",
+  storageBucket: "mern-auth-4b709.appspot.com",
+  messagingSenderId: "1052177974580",
+  appId: "1:1052177974580:web:f1b2c9c6a3ca2f10381161"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
